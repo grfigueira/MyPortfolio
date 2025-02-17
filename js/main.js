ca952e4e9eec156cc11a60a4c7d4e34a3e4e2a10
Guilherme Figueira <g.figueira.2002@gmail.com>
2025-02-17 03:09:41 +0000

    fix mistakes and made copyright year dynamic

--- js/main.js
+++ js/main.js
@@ -4,19 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const date = new Date();
 
-        const currentYear = date.getFullYear();
-        const currentMonth = date.getMonth();
-        const currentDay = date.getUTCDate();
+        const [currentYear, currentMonth, currentDay] = [date.getFullYear(), date.getMonth(), date.getUTCDate()];
 
         // no birthday yet
         if(currentMonth < birthMonth || (currentMonth + 1 == birthMonth && currentDay < birthDay))
         {
-            // print date
             console.log("current year: " + currentYear + " current month: " + currentMonth + " current day: " + currentDay);
             return currentYear - birthYear - 1;
         }
         return currentYear - birthYear;
     };
-    
+
     document.getElementById("age").textContent = calculateAge();
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
 });
